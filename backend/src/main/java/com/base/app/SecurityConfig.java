5d09466d8ed38be43ca8d8490c86d2fb07af9c1d
DavidNaranjo1237 <guillermo.naranjo@udea.edu.co>
2026-06-02 07:36:23 -0500

    fix: cors allow vercel frontend

--- backend/src/main/java/com/base/app/SecurityConfig.java
+++ backend/src/main/java/com/base/app/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
             "http://localhost:3000",
-            "http://localhost:5173"
+            "http://localhost:5173",
             "https://personal-finance-manager3.vercel.app"
         ));
 
